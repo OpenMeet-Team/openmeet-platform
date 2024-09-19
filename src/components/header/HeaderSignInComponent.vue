f066dc1e3cef8b6b8d912c507bb75530213bfd77
Stan Khatko <stanislavkhatko@gmail.com>
2024-09-18 19:04:22 -0500

    fix buttons

--- src/components/header/HeaderSignInComponent.vue
+++ src/components/header/HeaderSignInComponent.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 
+import { useAuthStore } from 'stores/auth-store.ts'
 </script>
 
 <template>
-  <div class="row q-gutter-md">
+  <div class="row q-gutter-md" v-if="!useAuthStore().isAuthenticated">
     <q-btn outline :to="{name: 'AuthLoginPage'}" label="Sign in"/>
     <q-btn color="secondary" :to="{name: 'AuthRegisterPage'}" label="Register"/>
 <!--    <MenuItemComponent label="Forgot password" :to="{name: 'AuthForgotPasswordPage'}"/>-->
