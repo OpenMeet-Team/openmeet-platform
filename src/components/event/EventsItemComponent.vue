95bad6ebf52e83b720bdcf8e82cb7e77d5a9fc11
Stan Khatko <stanislavkhatko@gmail.com>
2024-10-21 23:29:07 -0500

    chore: events item redesign

--- src/components/event/EventsItemComponent.vue
+++ src/components/event/EventsItemComponent.vue
@@ -13,20 +13,20 @@ const { navigateToEvent, navigateToGroup } = useNavigation()
 </script>
 
 <template>
-  <q-card flat class="event-card q-my-xl overflow-hidden">
-    <q-card-section horizontal>
-      <q-img class="cursor-pointer" style="width: 150px;" @click="navigateToEvent(event.slug, event.id)" :src="getImageSrc(event.image)">
-        <div class="q-pa-none absolute q-ml-sm no-padding bg-transparent"><q-badge>{{ event.type }}</q-badge></div>
-      </q-img>
-      <q-card-actions vertical class="q-px-md">
-        <div class="text-subtitle2 cursor-pointer" @click="navigateToEvent(event.slug, event.id)">{{ formatDate(event.startDate) }}</div>
-        <div class="text-h6 cursor-pointer ellipsis" @click="navigateToEvent(event.slug, event.id)">{{ event.name }}</div>
-        <div class="text-h5 cursor-pointer" v-if="event.group" @click="navigateToGroup(event.group.slug, event.group.id)">{{ event.group.name }}</div>
-        <div class="text-subtitle2">{{ event.location }}</div>
-        <div class="q-mt-sm text-body2"><q-icon name="sym_r_people" /> {{ event.attendeesCount }} <span v-if="event.maxAttendees">/ {{ event.maxAttendees }}</span> attendees</div>
-      </q-card-actions>
-    </q-card-section>
-  </q-card>
+    <div class="row q-my-md q-gutter-md wrap">
+    <q-img class="cursor-pointer rounded-borders" style="max-width: 300px; height: 150px;" @click="navigateToEvent(event.slug, event.id)" ratio="16/9" :src="getImageSrc(event.image)">
+      <div class="q-pa-none absolute q-ml-sm no-padding bg-transparent"><q-badge>{{ event.type }}</q-badge></div>
+    </q-img>
+    <div class="col column">
+      <div class="text-h5 text-bold q-pa-none cursor-pointer elipsys" @click="navigateToEvent(event.slug, event.id)">{{ formatDate(event.startDate) }}</div>
+      <div class="text-subtitle2">{{  event.name }}</div>
+      <div class="text-subtitle2">{{ event.location }}</div>
+      <div class="text-h5 cursor-pointer" v-if="event.group" @click="navigateToGroup(event.group.slug, event.group.id)">{{ event.group.name }}</div>
+
+      <q-space/>
+      <div class="q-mt-sm text-body2"><q-icon name="sym_r_people" /> {{ event.attendeesCount }} <span v-if="event.maxAttendees">/ {{ event.maxAttendees }}</span> attendees</div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
--- src/components/group/GroupsItemComponent.vue
+++ src/components/group/GroupsItemComponent.vue
@@ -15,12 +15,13 @@ const { navigateToGroup } = useNavigation()
 <template>
   <div class="row q-my-md q-gutter-md wrap">
     <q-img class="cursor-pointer rounded-borders" style="max-width: 300px; height: 150px;" @click="navigateToGroup(group.slug, group.id)" ratio="16/9" :src="getImageSrc(group.image)"/>
-    <div class="col">
-        <div class="text-h5 text-bold q-pa-none cursor-pointer elipsys" @click="navigateToGroup(group.slug, group.id)">{{ group.name }}</div>
-        <div class="text-subtitle2" v-if="group.categories">
+    <div class="col column">
+      <div class="text-h5 text-bold q-pa-none cursor-pointer elipsys" @click="navigateToGroup(group.slug, group.id)">{{ group.name }}</div>
+      <div class="text-subtitle2" v-if="group.categories">
           {{ group.categories.map((c: number | CategoryEntity) => typeof c === 'object' ? c.name : '').join(', ') }}
-        </div>
+      </div>
       <div class="text-subtitle2">{{ group.location }}</div>
+      <q-space/>
       <div class="q-mt-sm text-body2">
         <span v-if="group.groupMembersCount" class="q-mr-md"><q-icon name="sym_r_people" /> {{ group.groupMembersCount }} members</span>
         <q-badge>{{ group.visibility }}</q-badge>
