7732ad5f39a190206896b494db4b4d3de9c957bc
Tom Scanlan <tompscanlan@gmail.com>
2024-12-31 10:51:48 -0500

    update defaults

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ useMeta({
   title: '',
   titleTemplate: title => `${title} | ${getEnv('APP_TENANT_NAME') || 'OpenMeet'}`,
   meta: {
-    description: { content: getEnv('APP_TENANT_DESCRIPTION') || 'Building communities should be easy and free. OpenMeet is a free and opensource platform for creating and managing events, groups, and communities.' },
+    description: { content: getEnv('APP_TENANT_DESCRIPTION') || 'Managing social events should be easy and free. OpenMeet is a new, free and opensource platform for creating and managing events, groups and communities.' },
     'og:image': { content: getEnv('APP_TENANT_IMAGE') || '/openmeet/openmeet-logo.png' }
   }
 })
