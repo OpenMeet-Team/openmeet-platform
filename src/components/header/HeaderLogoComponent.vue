692b5370d18415873cfbfaf8d3310479f52b0153
Tom Scanlan <tompscanlan@gmail.com>
2025-01-10 16:54:30 -0500

    Tom/20250110 (#41)
    * update font
    
    * update logo more like biz
    
    * bring in more colors

--- src/components/header/HeaderLogoComponent.vue
+++ src/components/header/HeaderLogoComponent.vue
@@ -3,10 +3,13 @@
 </script>
 
 <template>
-  <q-toolbar-title class="c-header-logo-component row no-wrap items-center" data-cy="header-logo-component">
-    <router-link style="color: inherit; text-decoration: none" to="/" class="inline row no-wrap items-center router-link-inherit">
-      <q-img class="q-mr-xs" left src="~assets/openmeet/logo.svg" style="height: 35px; width: 35px" />
-      <div class="text-bold gt-sm">OpenMeet</div>
+  <q-toolbar-title class="c-header-logo-component" data-cy="header-logo-component">
+    <router-link to="/" class="self-center text-inherit no-decoration row items-center">
+      <q-img
+        src="~assets/openmeet/logo.svg"
+        style="height: 45px; width: 45px"
+      />
+      <div class="text-bold q-ml-sm text-h5">OpenMeet</div>
     </router-link>
   </q-toolbar-title>
 </template>
@@ -14,5 +17,13 @@
 <style scoped lang="scss">
 .q-toolbar__title {
   flex: none;
+}
+
+.no-decoration {
+  text-decoration: none;
+}
+
+.text-inherit {
+  color: inherit;
 }
 </style>
