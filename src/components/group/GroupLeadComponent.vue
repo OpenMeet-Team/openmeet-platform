5a41296ff85dd67220052372391873fa57f3ff21
Tom Scanlan <tompscanlan@gmail.com>
2025-05-11 12:29:12 -0400

    spelling

--- src/components/group/GroupLeadComponent.vue
+++ src/components/group/GroupLeadComponent.vue
@@ -37,7 +37,7 @@ const { navigateToMember } = useNavigation()
           </div>
           <div class="row items-start q-mt-xs" v-if="group.createdBy">
             <q-icon size="sm" left name="sym_r_person" class="text-purple-300"/>
-            <div class="text-body1 cursor-pointer">Organised by <span class="router-link-inherit" v-if="group.createdBy" @click.stop="navigateToMember(group.createdBy)">{{ group.createdBy.name }}</span></div>
+            <div class="text-body1 cursor-pointer">Organized by <span class="router-link-inherit" v-if="group.createdBy" @click.stop="navigateToMember(group.createdBy)">{{ group.createdBy.name }}</span></div>
           </div>
         </q-card-section>
 
