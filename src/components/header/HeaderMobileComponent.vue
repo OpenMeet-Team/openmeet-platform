ea3eb72f7131ee62a4f3d91a272c6bff6c79d7c5
Tom Scanlan <tom@openmeet.net>
2025-10-29 08:09:08 -0400

    fix: add non-breaking space before 'here' link in beta banner
    Ensures proper spacing before the 'here' link in beta banner on both
    desktop and mobile headers.
    
    Addresses #246

--- src/components/header/HeaderMobileComponent.vue
+++ src/components/header/HeaderMobileComponent.vue
@@ -7,7 +7,7 @@ import HeaderSearchComponent from './HeaderSearchComponent.vue'
 <template>
   <div class="c-header-mobile-component">
     <div class="xs-hide bg-dark-page text-white text-uppercase text-body2 text-bold row justify-center q-pa-sm" >
-      This platform is currently in beta. Report issues <a class="text-primary" href="https://github.com/openmeet-team/openmeet-platform/issues" target="_blank">here</a>.
+      This platform is currently in beta. Report issues&nbsp;<a class="text-primary" href="https://github.com/openmeet-team/openmeet-platform/issues" target="_blank">here</a>.
     </div>
 
     <q-toolbar>
